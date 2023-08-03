325aba7e89771034ebfd7b4a98b578984b819bf4
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-08-02 21:29:46 -0700

    Update setup.py

--- python/setup/setup.py
+++ python/setup/setup.py
@@ -1,5 +1,15 @@
-# import marvin
-# to use an OpenAI model (if not specified, defaults to gpt-4)
-# marvin.settings.openai.api_key = YOUR_API_KEY
+import marvin
+from marvin import ai_fn
 
-print("hello")
+marvin.settings.openai.api_key = "YOUR OPENAI KEY"
+
+@ai_fn
+def sentiment(text: str) -> float:
+    """
+    Given `text`, returns a number between 1 (positive) and -1 (negative)
+    indicating its sentiment score.
+    """
+
+
+print("Text 1:", sentiment("I love working with Marvin!"))
+print("Text 2:", sentiment("These examples could use some work..."))
