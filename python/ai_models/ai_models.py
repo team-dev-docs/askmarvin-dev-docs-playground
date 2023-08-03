f9e772da14d15e6d79bdb5a1a2cb3f1e8eb6a653
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-08-03 12:22:31 -0700

    added python/ai_models/ai_models.py

---
+++ python/ai_models/ai_models.py
@@ -0,0 +1,9 @@
+
+python
+from marvin import ai_model
+from pydantic import BaseModel, Field
+@ai_model
+class Location(BaseModel):
+    city: str
+    state: str = Field(..., description="The two-letter state abbreviation")
+Location("The Big Apple")
